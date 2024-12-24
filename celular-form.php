ba041cbffef8088f80c51720442ca4b6f3059762
Waltersiquira <167370881+Waltersiquira@users.noreply.github.com>
2024-12-24 14:24:28 -0300

    Create celular-form.php

---
+++ celular-form.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GuiCelulares</title>
+</head>
+<body>
+    <form action="enviar-form-celular.php" method="post">
+        digite nome do celular: <br>
+        <input type="text" name="nome"> <br>
+        digite as especificações do celular: <br>
+        <input type="text" name="especificacoes"> <br>
+        digite o preço do celular: <br>
+        <input type="text" name="preco"> <br>
+        digite a imagem do celular: <br>
+        <input type="text" name="imagem"> <br>
+        <input type="submit">
+    </form>
+</body>
+</html>
